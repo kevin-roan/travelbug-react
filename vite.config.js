332bab26e46191bb564866dc94e5d618c99d1bd9
Kevin Roan <kevinroan@proton.me>
2024-11-07 20:19:51 +0530

    testing prod isssue

--- vite.config.js
+++ vite.config.js
@@ -16,36 +16,51 @@
 // });
 //
 //
+// import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+//
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
+//
+//   return {
+//     plugins: [react()],
+//     server: {
+//       proxy: {
+//         "/api": {
+//           target: "https://iamanas.in/travel_bug/web_api/",
+//           changeOrigin: true,
+//           secure: true, // Set to true if your target is an HTTPS server
+//           rewrite: (path) => path.replace(/^\/api/, ""),
+//         },
+//       },
+//       cors: false, // Set this to true or false based on your needs
+//     },
+//     preview: {
+//       proxy: {
+//         "/api": {
+//           target: "https://iamanas.in/travel_bug/web_api/",
+//           changeOrigin: true,
+//           secure: true, // Set to true if your target is an HTTPS server
+//           rewrite: (path) => path.replace(/^\/api/, ""),
+//         },
+//       },
+//       cors: false, // Set this to true or false based on your needs
+//     },
+//   };
+// });
+//
+//
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: "https://iamanas.in/travel_bug/web_api/",
-          changeOrigin: true,
-          secure: false, // Set to true if your target is an HTTPS server
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-      cors: false, // Set this to true or false based on your needs
-    },
-    preview: {
-      proxy: {
-        "/api": {
-          target: "https://iamanas.in/travel_bug/web_api/",
-          changeOrigin: true,
-          secure: false, // Set to true if your target is an HTTPS server
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-      cors: false, // Set this to true or false based on your needs
+    define: {
+      "process.env.API_URL": JSON.stringify(env.VITE_API_URL),
     },
   };
 });
