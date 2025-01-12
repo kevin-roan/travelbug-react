aba4aff2e1b033d0db547ea77c5a83dc96104e0d
Kevin Roan <kevinroan@proton.me>
2025-01-12 21:55:49 +0530

    equiryform form submittion

--- src/components/EnquiryForm.jsx
+++ src/components/EnquiryForm.jsx
@@ -1,7 +1,91 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import axios from "axios";
+
+const successMessage = "We will contact you soon";
+const failedMessage =
+  "Sorry that failed,Try again or please contact us through the email address";
 
 export default function EnquiryForm() {
   const [isMinimized, setIsMinimized] = useState(false);
+  const [error, setError] = useState(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    city: "",
+    email: "",
+    phone: "",
+    whatsapp: "",
+    destination: "",
+    date: "",
+    no_of_people: "",
+    vacation_type: "",
+  });
+
+  const notify = (message) =>
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Bounce,
+    });
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `https://iamanas.in/travel_bug/web_api/insert_package_enquiry`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        },
+      );
+
+      if (response.status) {
+        setFormData({
+          username: "",
+          email: "",
+          package_type_id: "",
+          message: "",
+        });
+        setFormData({
+          name: "",
+          city: "",
+          email: "",
+          phone: "",
+          whatsapp: "",
+          destination: "",
+          date: "",
+          no_of_people: "",
+          vacation_type: "",
+        });
+
+        notify(successMessage);
+      }
+    } catch (error) {
+      setError(error.message);
+      console.log("Error submitting form:", error.request);
+      notify(failedMessage);
+    }
+  };
 
   const formStyles = {
     container: {
@@ -71,6 +155,7 @@ export default function EnquiryForm() {
       fontWeight: "bold",
       cursor: "pointer",
       transition: "background-color 0.3s ease",
+      zIndex: 2000,
     },
     flagIcon: {
       position: "absolute",
@@ -82,81 +167,123 @@ export default function EnquiryForm() {
   };
 
   return (
-    <div >
-      <button
-        style={formStyles.toggleButton}
-        onClick={() => setIsMinimized(!isMinimized)}
-      >
-        {isMinimized ? "→" : "←"}
-      </button>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <button
+          style={formStyles.toggleButton}
+          onClick={() => setIsMinimized(!isMinimized)}
+        >
+          {isMinimized ? "→" : "←"}
+        </button>
 
-      <h2 style={formStyles.title}>Stay Connected</h2>
+        <h2 style={formStyles.title}>Stay Connected</h2>
 
-      <div style={formStyles.inputGroup}>
-        <input type="text" placeholder="Name *" style={formStyles.input} />
-      </div>
+        <div style={formStyles.inputGroup}>
+          <input
+            type="text"
+            placeholder="Name *"
+            name="name"
+            style={formStyles.input}
+            onChange={handleInputChange}
+          />
+        </div>
 
-      <div style={formStyles.inputGroup}>
-        <input
-          type="text"
-          placeholder="City of Residence *"
-          style={formStyles.input}
+        <div style={formStyles.inputGroup}>
+          <input
+            type="text"
+            placeholder="City of Residence *"
+            style={formStyles.input}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div style={formStyles.inputGroup}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email *"
+            style={formStyles.input}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div style={formStyles.inputGroup}>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Phone Number *"
+            style={formStyles.phoneInput}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div style={formStyles.inputGroup}>
+          <input
+            type="tel"
+            name="whatsapp"
+            placeholder="WhatsApp"
+            style={formStyles.phoneInput}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div style={formStyles.inputGroup}>
+          <input
+            type="text"
+            name="destination"
+            placeholder="Travel Destination *"
+            style={formStyles.input}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div style={formStyles.inputGroup}>
+          <input
+            type="date"
+            name="date"
+            placeholder="Date of Travel *"
+            style={formStyles.input}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div style={formStyles.inputGroup}>
+          <input
+            type="number"
+            name="no_of_people"
+            placeholder="No. of People *"
+            style={formStyles.input}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div style={formStyles.inputGroup}>
+          <input
+            type="text"
+            name="vacation_type"
+            placeholder="Vacation Type *"
+            style={formStyles.input}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <button style={formStyles.submitButton} type="submit">
+          SUBMIT
+        </button>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+          transition={Bounce}
         />
-      </div>
-
-      <div style={formStyles.inputGroup}>
-        <input type="email" placeholder="Email *" style={formStyles.input} />
-      </div>
-
-      <div style={formStyles.inputGroup}>
-        <input
-          type="tel"
-          placeholder="Phone Number *"
-          style={formStyles.phoneInput}
-        />
-      </div>
-
-      <div style={formStyles.inputGroup}>
-        <input
-          type="tel"
-          placeholder="WhatsApp"
-          style={formStyles.phoneInput}
-        />
-      </div>
-
-      <div style={formStyles.inputGroup}>
-        <input
-          type="text"
-          placeholder="Travel Destination *"
-          style={formStyles.input}
-        />
-      </div>
-
-      <div style={formStyles.inputGroup}>
-        <input
-          type="date"
-          placeholder="Date of Travel *"
-          style={formStyles.input}
-        />
-      </div>
-
-      <div style={formStyles.inputGroup}>
-        <input
-          type="number"
-          placeholder="No. of People *"
-          style={formStyles.input}
-        />
-      </div>
-
-      <div style={formStyles.inputGroup}>
-        <input
-          type="text"
-          placeholder="Vacation Type *"
-          style={formStyles.input}
-        />
-      </div>
-
-      <button style={formStyles.submitButton}>SUBMIT</button>
+      </form>
     </div>
   );
 }
